b09b88395f4f21cca18b63d3da2899331b452fd0
AndrewVoroshen <voroshen.av@gmail.com>
2017-11-07 21:52:33 +0300

    feat: aop logging(still not working)

---
+++ src/main/java/com/instinctools/padlaboris/aspect/LoggingAdvisor.java
@@ -0,0 +1,28 @@
+package com.instinctools.padlaboris.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class LoggingAdvisor {
+
+    private final Auditing auditing;
+
+    @Autowired
+    public LoggingAdvisor(final Auditing auditing) {
+        this.auditing = auditing;
+    }
+
+    @Before("com.instinctools.padlaboris.aspect.LoggingPointcut.logService()")
+    public void serviceLogAdvice(final JoinPoint joinPoint) {
+        auditing.logService(joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(),
+                Arrays.toString(joinPoint.getArgs()));
+    }
+}
